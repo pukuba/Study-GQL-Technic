1383c16f68ce5635bdb3b138e574881aaaae79e3
SeungWon <20sunrin041@sunrint.hs.kr>
2021-01-07 00:12:50 +0900

    added dataloaders

--- src/resolvers/Type.ts
+++ src/resolvers/Type.ts
@@ -1,9 +1,9 @@
 import { Db } from 'mongodb'
 
 const Post = {
-    comments: async (parent: any, args: any, { db }: { db: Db }) => {
-        const comment = await db.collection('comment').find({ postId: parent.postId }).toArray()
-        return comment
+    comments: async (parent: any, args: any, { db, loaders }: { db: Db, loaders: any }) => {
+        // const comment = await db.collection('comment').find({ postId: parent.postId }).toArray()
+        return loaders.commentsLoader.load(parent.postId)
     }
 }
 
