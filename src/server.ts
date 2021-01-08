fa65e8c4049e9316ad9c52289e3cb43582d2cb2b
SeungWon <20sunrin041@sunrint.hs.kr>
2021-01-09 03:09:23 +0900

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { createServer } from 'http'
 import dotenv from 'dotenv'
 dotenv.config()
 import path from 'path'
-import responseCachePlugin from 'apollo-server-plugin-response-cache'
+// import responseCachePlugin from 'apollo-server-plugin-response-cache'
 import { commentsLoader } from './lib/dataloader'
 import DB from './config/connectDB'
 import depthLimit from 'graphql-depth-limit'
